0a7d559edb09b0057adc6a71b9d37a26066c46d0
CSParadiso <cayetanosimonparadiso@protonmail.com>
2026-06-26 20:10:45 -0300

    feat: añadido comportamiento básico de cámara.

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,17 +1,62 @@
-import { useRouter } from "expo-router";
-import { Button, Linking, StyleSheet, Text, View } from "react-native";
+import { CameraType, CameraView, useCameraPermissions } from "expo-camera";
+import { useState } from "react";
+import {
+  Button,
+  Linking,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-export default function SearchScreen() {
-  const router = useRouter();
+export default function CameraScreen() {
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [permission, requestPermission] = useCameraPermissions();
+
+  console.log("### permission", permission);
+
+  const goToSettings = () => {
+    // Open the app settings to allow the user to grant camera permissions.
+    Linking.openSettings();
+  };
+
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted && permission.canAskAgain) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>Ir a ajustes de permisos</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  if (!permission.granted && !permission.canAskAgain) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>Ir a ajustes de permisos</Text>
+        <Button onPress={goToSettings} title="Go to Settings" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Búsqueda</Text>
-      <Text style={styles.description}>
-        Aquí se podrá buscar alimentos con el código de barras.
-      </Text>
-      <Button 
-        title="Mientras tanto" 
-       onPress={() => Linking.openURL("https://paradisoft.com.ar")}/>
+      <CameraView style={styles.camera} facing={facing} />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+          <Text style={styles.text}>Cambiar Cámara</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -20,19 +65,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
-    padding: 24,
-    gap: 12,
-    backgroundColor: "#eff6ff",
   },
-  title: {
-    fontSize: 32,
-    fontWeight: "700",
-    color: "#1d4ed8",
-  },
-  description: {
-    fontSize: 18,
+  message: {
     textAlign: "center",
-    color: "#1e3a8a",
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: 64,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    width: "100%",
+    paddingHorizontal: 64,
+  },
+  button: {
+    flex: 1,
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
   },
 });
